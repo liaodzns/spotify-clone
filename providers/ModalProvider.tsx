39e2c6ec206ee691ab25d9565375ae2781c1b255
Daniel Liao <danielliao@Daniels-MacBook-Pro.local>
2023-10-15 20:55:14 -0400

    final

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import AuthModal from "@/components/AuthModal"
+import Modal from "@/components/Modal"
+import SubscribeModal from "@/components/SubscribeModal"
+import UploadModal from "@/components/UploadModal"
+import { useEffect, useState } from "react"
+import { ProductWithPrice } from '@/types';
+
+interface ModalProviderProps {
+    products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({
+    products
+}) => {
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if (!isMounted) {
+        return null
+    }
+
+    return (
+        <>
+            <AuthModal/>
+            <SubscribeModal products={products} />
+            <UploadModal/>
+
+        </>
+    )
+}
+
+export default ModalProvider
